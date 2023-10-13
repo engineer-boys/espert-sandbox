afdd7bfb939865204405dca127e4788c0c0693bb
obroslakjakub <obroslakjakub@gmail.com>
2023-10-13 21:43:26 +0200

    configure scripts: allow specifying build type

--- unix-configure.sh
+++ unix-configure.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [[ $# == 0  || $1 == "debug" || $1 == "Debug" ]] ; then
+    BUILD_TYPE=Debug
+elif [[ $1 == "release" || "$1" == "Release" ]] ; then
+    BUILD_TYPE=Release
+else
+    echo "Invalid arguments. Usage: unix-configure.sh [debug|Debug|release|Release]"
+    exit 1
+fi
+
 SYSTEM_WSI=$(loginctl show-session $(loginctl | grep $(whoami) | awk '{print $1}') -p Type | cut -d'=' -f2)
 
 case $SYSTEM_WSI in
@@ -18,4 +27,4 @@ case $SYSTEM_WSI in
 esac
 
 mkdir -p build
-cmake -S . -B build -DVKB_WSI_SELECTION=$VKB_WSI
+cmake -S . -B build -DVKB_WSI_SELECTION=$VKB_WSI -DCMAKE_BUILD_TYPE=$BUILD_TYPE
--- windows-configure.bat
+++ windows-configure.bat
@@ -1,3 +1,9 @@
 if not exist build mkdir build
 
-cmake -S . -B build
+if %1 == "" set BUILD_TYPE="Debug"
+if %1 == "debug" set BUILD_TYPE="Debug"
+if %1 == "Debug" set BUILD_TYPE="Debug"
+if %1 == "release" set BUILD_TYPE="Release"
+if %1 == "Release" set BUILD_TYPE="Release"
+
+cmake -S . -B build -DCCMAKE_BUILD_TYPE=%BUILD_TYPE%
