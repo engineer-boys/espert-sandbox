81bf250d37f2f6a30a1ee6604dd1e655cbf10164
DawidSzpejna <dawid.szpejna13@gmail.com>
2023-10-13 19:37:27 +0200

    layer: enable layer infrastructure for client

--- main.cc
+++ main.cc
@@ -1,7 +1,40 @@
 #include <Espert.hh>
 
+class CrazyLayer : public Espert::Layer
+{
+    virtual ~CrazyLayer() override {}
+
+    virtual void attach() override {}
+
+    virtual void detach() override {}
+
+    virtual void update() override {}
+
+    virtual void handle_event(Espert::Event &event) override {
+        Espert::Event::
+            try_hanlder<Espert::KeyPressedEvent>(event, ESP_BIND_EVENT_FOR_FUN(CrazyLayer::my_crazy_event_handler));
+    }
+
+    bool my_crazy_event_handler(Espert::KeyPressedEvent &event)
+    {
+        // implementation ...
+        ESP_INFO("You pressed the button {0}", event.get_code());
+
+        /*
+            true - propagate to lower layers
+            false - this is the last layer of this event
+        */
+        return true;
+    }
+};
+
 class SandBoxApp : public Espert::Application
 {
+public:
+    SandBoxApp()
+    {
+        push_layer(new CrazyLayer());
+    }
 };
 
 Espert::Application *Espert::createAppInstance()
