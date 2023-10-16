985d203ae68b63607bad4144d1872a529f0ea1db
kubo11 <jakub.obroslak@onet.pl>
2023-10-16 15:48:11 +0200

    coding style: reformat all source files after .clang-format change

--- main.cc
+++ main.cc
@@ -1,6 +1,7 @@
 #include <Espert.hh>
 
-class CrazyLayer : public Espert::Layer {
+class CrazyLayer : public Espert::Layer
+{
   virtual ~CrazyLayer() override {}
 
   virtual void attach() override {}
@@ -9,12 +10,14 @@ class CrazyLayer : public Espert::Layer {
 
   virtual void update() override {}
 
-  virtual void handle_event(Espert::Event& event) override {
+  virtual void handle_event(Espert::Event& event) override
+  {
     Espert::Event::try_hanlder<Espert::KeyPressedEvent>(
         event, ESP_BIND_EVENT_FOR_FUN(CrazyLayer::my_crazy_event_handler));
   }
 
-  bool my_crazy_event_handler(Espert::KeyPressedEvent& event) {
+  bool my_crazy_event_handler(Espert::KeyPressedEvent& event)
+  {
     // implementation ...
     ESP_INFO("You pressed the button {0}", event.get_code());
 
@@ -26,11 +29,13 @@ class CrazyLayer : public Espert::Layer {
   }
 };
 
-class SandBoxApp : public Espert::Application {
+class SandBoxApp : public Espert::Application
+{
   public:
   SandBoxApp() { push_layer(new CrazyLayer()); }
 };
 
-Espert::Application* Espert::createAppInstance() {
+Espert::Application* Espert::createAppInstance()
+{
   return new SandBoxApp();
 }
