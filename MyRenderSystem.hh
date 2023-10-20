08287555eb5ddf09479387ceaf257ea88bfd9dfc
sebastianSzarafin <sebastian.szarafin@interia.pl>
2023-10-21 00:55:11 +0200

    uniforms: update sand box example to use uniform buffer

--- MyRenderSystem.hh
+++ MyRenderSystem.hh
@@ -8,58 +8,116 @@
 
 namespace my_game
 {
-	class MyRenderSystem
-	{
-	 private:
-		struct MyPushConstantData : esp::EspPushConstantData
-		{
-			glm::mat4 transform{1.0f};
-		};
+  class MyRenderSystem
+  {
+   private:
+    struct MyUniformData
+    {
+      glm::mat4 camera_projection{1.f};
+    };
 
-		esp::EspDevice& m_device;
-		std::unique_ptr<esp::EspPipelineLayout> m_pipeline_layout;
-		std::unique_ptr<esp::EspPipeline> m_pipeline;
+    struct MyPushConstantData : esp::EspPushConstantData
+    {
+            glm::mat4 transform{1.f};
+    };
 
-	 public:
-		MyRenderSystem() : m_device{esp::EspRenderContext::get_device()}
-		{
-			esp::PipelineConfigInfo pipeline_config{};
-			esp::EspPipeline::default_pipeline_config_info(pipeline_config);
+    esp::EspDevice& m_device;
 
-			esp::EspPipeline::Builder builder{esp::EspRenderContext::get_device()};
-			builder
-				.set_vert_shader_path("../shaders/shader.vert.spv")
-				.set_frag_shader_path("../shaders/shader.frag.spv");
+    std::vector<std::unique_ptr<esp::EspBuffer>> m_uniform_buffers{};
+    std::unique_ptr<esp::EspDescriptorSetLayout> m_descriptor_set_layout{};
+    std::vector<std::unique_ptr<esp::EspDescriptorSet>> m_descriptor_sets{};
+    std::unique_ptr<esp::EspDescriptorPool> m_descriptor_pool{};
 
-			m_pipeline_layout = builder.build_pipeline_layout(
-				pipeline_config,
-				esp::EspPushConstantData::create_range<MyPushConstantData>());
-			m_pipeline = builder.build_pipeline(
-				pipeline_config,
-				esp::EspRenderContext::get_scheduler().get_swap_chain_render_pass());
-		}
+    std::unique_ptr<esp::EspPipelineLayout> m_pipeline_layout;
+    std::unique_ptr<esp::EspPipeline> m_pipeline;
 
-		~MyRenderSystem()
-		{
-			m_pipeline_layout->destroy(esp::EspRenderContext::get_device());
-		}
+   public:
+    MyRenderSystem() : m_device{esp::EspRenderContext::get_device()}
+    {
+      /* -------------------- Configure descriptors -------------------- */
+      m_descriptor_pool =
+        esp::EspDescriptorPool::Builder(m_device)
+          .set_max_sets(esp::EspSwapChain::MAX_FRAMES_IN_FLIGHT)
+          .add_pool_size(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, esp::EspSwapChain::MAX_FRAMES_IN_FLIGHT)
+          .build();
 
-		MyRenderSystem(const MyRenderSystem&) = delete;
-		MyRenderSystem& operator=(const MyRenderSystem&) = delete;
+      m_uniform_buffers.resize(esp::EspSwapChain::MAX_FRAMES_IN_FLIGHT);
+      for (auto& buffer : m_uniform_buffers)
+      {
+        buffer = std::make_unique<esp::EspBuffer>(
+            m_device,
+            sizeof(MyUniformData),
+            1,
+            VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+            VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT);
+        buffer->map();
+      }
 
-		void render(VkCommandBuffer command_buffer, MyGameObject& game_object)
-		{
-			m_pipeline->bind(command_buffer);
+      m_descriptor_set_layout =
+        esp::EspDescriptorSetLayout::Builder(m_device)
+          .add_binding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS)
+          .build();
 
-			MyPushConstantData push{};
-			push.transform = game_object.m_transform.get_transform_mat();
+      m_descriptor_sets.resize(esp::EspSwapChain::MAX_FRAMES_IN_FLIGHT);
+      for (int i = 0; i < m_descriptor_sets.size(); i++)
+      {
+        auto buffer_info = m_uniform_buffers[i]->descriptor_info();
+        m_descriptor_sets[i] = std::make_unique<esp::EspDescriptorSet>();
+        esp::EspDescriptorWriter(*m_descriptor_set_layout, *m_descriptor_pool)
+            .write_buffer(0, &buffer_info)
+            .build(*m_descriptor_sets[i]);
+      }
 
-			push.bind<MyPushConstantData>(command_buffer, m_pipeline_layout->get_layout());
+      /* -------------------- Create pipeline -------------------- */
+      esp::PipelineConfigInfo pipeline_config{};
+      esp::EspPipeline::default_pipeline_config_info(pipeline_config);
 
-			game_object.m_model->bind(command_buffer);
-			game_object.m_model->draw(command_buffer);
-		}
-	};
+      esp::EspPipeline::Builder builder{esp::EspRenderContext::get_device()};
+      builder
+              .set_vert_shader_path("../shaders/shader.vert.spv")
+              .set_frag_shader_path("../shaders/shader.frag.spv");
+
+      m_pipeline_layout = builder.build_pipeline_layout(
+              pipeline_config,
+          m_descriptor_set_layout->get_layout(),
+              esp::EspPushConstantData::create_range<MyPushConstantData>());
+      m_pipeline = builder.build_pipeline(
+              pipeline_config,
+              esp::EspRenderContext::get_scheduler().get_swap_chain_render_pass());
+    }
+
+    ~MyRenderSystem()
+    {
+      m_pipeline_layout->destroy(esp::EspRenderContext::get_device());
+    }
+
+    MyRenderSystem(const MyRenderSystem&) = delete;
+    MyRenderSystem& operator=(const MyRenderSystem&) = delete;
+
+    void render(VkCommandBuffer command_buffer, MyGameObject& game_object, esp::renderer::Camera& camera)
+    {
+      m_pipeline->bind(command_buffer);
+
+      /* -------------------- Set uniform data -------------------- */
+      MyUniformData uniform{};
+      uniform.camera_projection = camera.get_projection();
+
+      int frame_index = esp::EspRenderContext::get_scheduler().get_current_frame_index();
+      m_descriptor_sets[frame_index]->bind(command_buffer, m_pipeline_layout->get_layout());
+      m_uniform_buffers[frame_index]->write_to_buffer(&uniform);
+      m_uniform_buffers[frame_index]->flush();
+
+      /* -------------------- Set push data -------------------- */
+      MyPushConstantData push{};
+      push.transform = game_object.m_transform.get_transform_mat();
+
+      push.bind<MyPushConstantData>(command_buffer, m_pipeline_layout->get_layout());
+
+      /* -------------------- Bind & Draw -------------------- */
+      game_object.m_model->bind(command_buffer);
+      game_object.m_model->draw(command_buffer);
+    }
+  };
 }
 
 #endif //ESPERT_SANDBOX__MY_RENDER_SYSTEM_H_
--- shaders/shader.vert
+++ shaders/shader.vert
@@ -7,12 +7,16 @@ layout (location = 3) in vec2 uv;
 
 layout(location = 0) out vec3 fragColor;
 
+layout (set = 0, binding = 0) uniform Ubo {
+    mat4 camera_projection;
+} ubo;
+
 layout(push_constant) uniform Push {
     mat4 transform;
 } push;
 
 void main()
 {
-    gl_Position = push.transform * vec4(position, 1.0);
+    gl_Position = ubo.camera_projection * push.transform * vec4(position, 1.0);
     fragColor = color;
 }
