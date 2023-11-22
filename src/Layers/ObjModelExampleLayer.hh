deb4c4561ebc74d23d67c03684ee94e3bd5c80d8
sebastianSzarafin <sebastian.szarafin@interia.pl>
2023-11-22 19:02:20 +0100

    scene: update obj model example to use scene graph

--- src/Layers/ObjModelExampleLayer.hh
+++ src/Layers/ObjModelExampleLayer.hh
@@ -17,55 +17,36 @@ namespace my_game
     glm::mat4 proj;
   };
 
-  class VikingRoom
-  {
-   public:
-    TransformComponent m_transform;
-    std::unique_ptr<ModelComponent> m_model;
-
-    VikingRoom()
-    {
-      ModelComponent::Builder builder{};
-      builder.load_model("../resources/Models/viking_room.obj");
-      m_model = std::make_unique<ModelComponent>(builder);
-    }
-
-    VikingRoomUniform update(Camera& camera, float dt)
-    {
-      auto& rot_y = m_transform.m_rotation.y;
-      rot_y       = glm::mod(rot_y + dt / 5, glm::two_pi<float>());
-
-      camera.set_perspective(EspFrameManager::get_swap_chain_extent_aspect_ratio());
-
-      VikingRoomUniform ubo{};
-      ubo.model = m_transform.get_model_mat();
-      ubo.view  = camera.get_view();
-      ubo.proj  = camera.get_projection();
-
-      return ubo;
-    }
-  };
-
   class ObjModelExampleLayer : public Layer
   {
    private:
     std::unique_ptr<EspPipeline> m_pipeline;
     std::unique_ptr<EspUniformManager> m_uniform_manager;
 
+    std::shared_ptr<Scene> m_scene;
     Camera m_camera{};
 
-    VikingRoom m_viking_room;
+    std::shared_ptr<SceneNode> m_viking_room_node;
 
    public:
     ObjModelExampleLayer()
     {
+      m_scene            = Scene::create();
+      m_viking_room_node = SceneNode::create();
+      m_scene->get_root().add_child(m_viking_room_node);
+
+      auto viking_room                                            = m_scene->create_entity("viking room");
+      viking_room->add_component<TransformComponent>().m_rotation = { glm::radians(90.f), glm::radians(-45.f), 0.0f };
+      ModelComponent::Builder model_builder{};
+      model_builder.load_model("../resources/Models/viking_room.obj"); // TODO: return Builder&
+      viking_room->add_component<ModelComponent>(model_builder);
+
+      m_viking_room_node->attach_entity(viking_room);
+
       m_camera.set_position(glm::vec3(0.0f, -2.0f, 3.0f));
       m_camera.look_at(glm::vec3(0.0f, 0.0f, 0.0f));
       m_camera.set_move_speed(3.f);
       m_camera.set_sensitivity(4.f);
-
-      m_viking_room                        = VikingRoom();
-      m_viking_room.m_transform.m_rotation = { glm::radians(90.f), glm::radians(-45.f), 0.0f };
 
       auto uniform_meta_data = EspUniformMetaData::create();
       uniform_meta_data->establish_descriptor_set();
@@ -87,19 +68,37 @@ namespace my_game
       m_uniform_manager->build();
     }
 
+    ~ObjModelExampleLayer() override { m_scene->destroy_entity(m_viking_room_node->get_entity()); }
+
    private:
     virtual void update(float dt) override
     {
       Camera::set_current_camera(&m_camera);
 
       m_pipeline->attach();
-      m_viking_room.m_model->attach();
 
-      auto ubo = m_viking_room.update(m_camera, dt);
+      auto& model = m_viking_room_node->get_entity().get_component<ModelComponent>();
+      model.attach();
+
+      m_viking_room_node->act(
+          [dt](Entity& e)
+          {
+            auto& transform = e.get_component<TransformComponent>();
+            auto& rot_y     = transform.m_rotation.y;
+            rot_y           = glm::mod(rot_y + dt / 5, glm::two_pi<float>());
+          });
+
+      m_camera.set_perspective(EspFrameManager::get_swap_chain_extent_aspect_ratio());
+
+      auto& transform = m_viking_room_node->get_entity().get_component<TransformComponent>();
+      VikingRoomUniform ubo{};
+      ubo.model = transform.get_model_mat();
+      ubo.view  = m_camera.get_view();
+      ubo.proj  = m_camera.get_projection();
       m_uniform_manager->update_buffer_uniform(0, 0, 0, sizeof(VikingRoomUniform), &ubo);
       m_uniform_manager->attach();
 
-      EspCommandHandler::draw_indexed(m_viking_room.m_model->get_index_count());
+      EspCommandHandler::draw_indexed(model.get_index_count());
     }
   };
 
