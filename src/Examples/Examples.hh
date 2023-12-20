82ef9fdcbeffb832ced75c2b9fc3830952288b5e
sebastianSzarafin <sebastian.szarafin@interia.pl>
2023-12-20 21:01:58 +0100

    examples: move examples to examples dir

---
+++ src/Examples/Examples.hh
@@ -0,0 +1,13 @@
+#ifndef EXAMPLES_EXAMPLES_HH
+#define EXAMPLES_EXAMPLES_HH
+
+#include "Examples/ExampleLayer.hh"
+#include "Examples/InstancingExampleLayer.hh"
+#include "Examples/ModelExample/ModelExampleLayer.hh"
+#include "Examples/ObjExample/BackpackObjModelExampleLayer.hh"
+#include "Examples/ObjExample/VikingRoomObjModelExampleLayer.hh"
+#include "Examples/OffscreenRenderingLayer.hh"
+#include "Examples/TextureDepthExampleLayer.hh"
+#include "Examples/TextureExampleLayer.hh"
+
+#endif // EXAMPLES_EXAMPLES_HH
