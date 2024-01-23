f6b721fa23ebe1c42646e2b55ee21f6f45a25e06
sebastianSzarafin <sebastian.szarafin@interia.pl>
2024-01-23 18:48:26 +0100

    examples: update animated model example for scene system

--- src/Examples/AdvanceModel/AnimatedModelLayer.hh
+++ src/Examples/AdvanceModel/AnimatedModelLayer.hh
@@ -18,15 +18,15 @@ namespace advance_model
   class AnimatedModelLayer : public Layer
   {
    private:
+    std::shared_ptr<Scene> m_scene;
     Camera m_camera{};
+    std::shared_ptr<Animator> m_animator;
 
     struct
     {
-      std::shared_ptr<EspShader> m_shader;
-      std::unique_ptr<EspUniformManager> m_uniform_managers;
-      std::unique_ptr<EspUniformManager> m_uniform_managers_material;
-      std::unique_ptr<NModel> m_model;
-    } m_sphere;
+      std::shared_ptr<Node> m_node;
+      std::shared_ptr<NModel> m_model;
+    } m_cesium_man;
 
     struct
     {
@@ -34,17 +34,10 @@ namespace advance_model
       std::shared_ptr<EspDepthBlock> m_depth_block;
     } m_final_pass;
 
-    std::shared_ptr<Animator> m_animator;
-    NModelParams m_params;
 
    public:
     AnimatedModelLayer()
     {
-      m_camera.set_position(glm::vec3(0.0f, 1.0f, 2.0f));
-      m_camera.look_at(glm::vec3(0.0f, 1.2f, 0.0f));
-      m_camera.set_move_speed(3.f);
-      m_camera.set_sensitivity(4.f);
-
       // final pass
       {
         m_final_pass.m_depth_block = EspDepthBlock::build(EspDepthBlockFormat::ESP_FORMAT_D32_SFLOAT,
@@ -55,78 +48,74 @@ namespace advance_model
         m_final_pass.m_final_product_plan->build();
       }
 
-      // model
+      // create shader
+      NModelParams model_params = { .m_position                = true,
+                                    .m_color                   = true,
+                                    .m_normal                  = true,
+                                    .m_tex_coord               = true,
+                                    .m_bone_ids                = true,
+                                    .m_weights                 = true,
+                                    .m_tangent                 = false,
+                                    .m_material_texture_layout = { { 1, 0, EspTextureType::ALBEDO } } };
+
+      auto uniform_meta_data = EspUniformMetaData::create();
+      uniform_meta_data->establish_descriptor_set();
+      uniform_meta_data->add_buffer_uniform(EspUniformShaderStage::ESP_VTX_STAGE, sizeof(PMVLV2));
+      uniform_meta_data->add_buffer_uniform(EspUniformShaderStage::ESP_VTX_STAGE, 100 * sizeof(glm::mat4));
+      uniform_meta_data->establish_descriptor_set();
+      uniform_meta_data->add_texture_uniform(EspUniformShaderStage::ESP_FRAG_STAGE);
+
+      auto shader = ShaderSystem::acquire("Shaders/AnimatedModel/shader");
+      shader->enable_depth_test(EspDepthBlockFormat::ESP_FORMAT_D32_SFLOAT, EspCompareOp::ESP_COMPARE_OP_LESS_OR_EQUAL);
+      shader->set_vertex_layouts({ model_params.get_vertex_layouts() });
+      shader->set_worker_layout(std::move(uniform_meta_data));
+      shader->build_worker();
+
+      // create scene
       {
-        m_params = { .m_position                = true,
-                     .m_color                   = true,
-                     .m_normal                  = true,
-                     .m_tex_coord               = true,
-                     .m_bone_ids                = true,
-                     .m_weights                 = true,
-                     .m_tangent                 = false,
-                     .m_material_texture_layout = { { 1, 0, EspTextureType::ALBEDO } } };
+        m_scene             = Scene::create();
+        m_cesium_man.m_node = Node::create();
+        m_scene->get_root().add_child(m_cesium_man.m_node);
 
-        auto uniform_meta_data = EspUniformMetaData::create();
-        uniform_meta_data->establish_descriptor_set();
-        uniform_meta_data->add_buffer_uniform(EspUniformShaderStage::ESP_VTX_STAGE, sizeof(PMVLV2));
-        uniform_meta_data->add_buffer_uniform(EspUniformShaderStage::ESP_VTX_STAGE, 100 * sizeof(glm::mat4));
-        uniform_meta_data->establish_descriptor_set();
-        uniform_meta_data->add_texture_uniform(EspUniformShaderStage::ESP_FRAG_STAGE);
+        m_camera.set_position(glm::vec3(0.0f, 1.0f, 2.0f));
+        m_camera.look_at(glm::vec3(0.0f, 1.2f, 0.0f));
+        m_camera.set_move_speed(3.f);
+        m_camera.set_sensitivity(4.f);
 
-        m_sphere.m_shader = ShaderSystem::acquire("Shaders/AnimatedModel/shader");
-        m_sphere.m_shader->enable_depth_test(EspDepthBlockFormat::ESP_FORMAT_D32_SFLOAT,
-                                             EspCompareOp::ESP_COMPARE_OP_LESS_OR_EQUAL);
-        m_sphere.m_shader->set_vertex_layouts({ m_params.get_vertex_layouts() });
-        m_sphere.m_shader->set_worker_layout(std::move(uniform_meta_data));
-        m_sphere.m_shader->build_worker();
+        m_cesium_man.m_model = std::make_shared<NModel>("AdvanceModels/CesiumMan/CesiumMan.gltf", model_params);
+        auto cesium_man      = m_scene->create_entity("cesium man");
+        cesium_man->add_component<NModelComponent>(m_cesium_man.m_model, shader);
+        m_cesium_man.m_node->attach_entity(cesium_man);
 
-        {
-          m_sphere.m_uniform_managers = m_sphere.m_shader->create_uniform_manager(0, 0);
-          m_sphere.m_uniform_managers->build();
-        }
-
-        m_sphere.m_model = std::make_unique<NModel>("AdvanceModels/CesiumMan/CesiumMan.gltf", m_params);
-        m_sphere.m_uniform_managers_material =
-            m_sphere.m_model->m_meshes[0].m_material->create_uniform_manager(m_sphere.m_shader);
-
-        m_animator = std::shared_ptr<Animator>{ new Animator() };
-        m_animator->play_animation(m_sphere.m_model.get(), 0, AnimationPeriodType::MANY_TIMES, 3);
+        m_animator = std::make_shared<Animator>();
+        m_animator->play_animation(m_cesium_man.m_model.get(), 0, AnimationPeriodType::MANY_TIMES, 3);
       }
     }
 
    private:
     virtual void update(float dt) override
     {
-      m_animator->update_animation(dt, m_sphere.m_model.get());
+      m_animator->update_animation(dt, m_cesium_man.m_model.get()); // TODO: pass model by reference (?)
 
       Scene::set_current_camera(&m_camera);
       m_camera.set_perspective(EspWorkOrchestrator::get_swap_chain_extent_aspect_ratio());
 
       m_final_pass.m_final_product_plan->begin_plan();
       {
-        m_sphere.m_shader->attach();
-
+        // update
         PMVLV2 pmvlv{};
         pmvlv.projection = m_camera.get_projection();
         pmvlv.view       = m_camera.get_view();
 
-        m_sphere.m_uniform_managers->update_buffer_uniform(0, 0, 0, sizeof(PMVLV2), &pmvlv);
+        auto& uniform_manager =
+            m_cesium_man.m_node->get_entity()->get_component<NModelComponent>().get_uniform_manager();
+        uniform_manager.update_buffer_uniform(0, 0, 0, sizeof(PMVLV2), &pmvlv);
 
         auto matrices = m_animator->get_final_bone_matrices();
-        m_sphere.m_uniform_managers->update_buffer_uniform(0, 1, 0, 100 * sizeof(glm::mat4), matrices);
-        m_sphere.m_uniform_managers->attach();
+        uniform_manager.update_buffer_uniform(0, 1, 0, 100 * sizeof(glm::mat4), matrices);
 
-        m_sphere.m_uniform_managers_material->attach();
-
-        for (auto node : *(m_sphere.m_model))
-        {
-          for (auto& mesh_idx : node.m_current_node->m_meshes)
-          {
-            EspJob::draw_indexed(m_sphere.m_model->m_meshes[mesh_idx].m_index_count,
-                                 1,
-                                 m_sphere.m_model->m_meshes[mesh_idx].m_first_index);
-          }
-        }
+        // render
+        m_scene->draw();
       }
       m_final_pass.m_final_product_plan->end_plan();
 
