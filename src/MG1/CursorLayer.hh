2f07c0adbb61a4ef6db60ddded8ac2b45f6a8b60
Sebastian Szarafin <sebastian.szarafin@interia.pl>
2024-03-11 22:20:05 +0100

    MG1: add ubo for cursor params

--- src/MG1/CursorLayer.hh
+++ src/MG1/CursorLayer.hh
@@ -3,6 +3,9 @@
 
 #include "Espert.hh"
 
+#define CURSOR_SIZE  0.025f
+#define CURSOR_COLOR glm::vec3(1, 1, 1)
+
 using namespace esp;
 
 namespace mg1
@@ -10,6 +13,13 @@ namespace mg1
   class CursorLayer : public Layer
   {
    private:
+    struct CursorParams
+    {
+      alignas(16) float m_cursor_size;
+      alignas(16) glm::vec3 m_cursor_color;
+      float m_aspect_ratio;
+    };
+
     std::shared_ptr<EspShader> m_shader;
     std::unique_ptr<EspUniformManager> m_uniform_manager;
 
@@ -20,8 +30,9 @@ namespace mg1
       {
         auto uniform_meta_data = EspUniformMetaData::create();
         uniform_meta_data->establish_descriptor_set();
+        uniform_meta_data->add_buffer_uniform(EspUniformShaderStage::ESP_ALL_STAGES, sizeof(CursorParams));
+        uniform_meta_data->establish_descriptor_set();
         uniform_meta_data->add_buffer_uniform(EspUniformShaderStage::ESP_VTX_STAGE, sizeof(glm::vec2));
-        uniform_meta_data->add_push_uniform(EspUniformShaderStage::ESP_VTX_STAGE, 0, sizeof(glm::mat4));
 
         m_shader = ShaderSystem::acquire("Shaders/MG1/CursorLayer/shader");
         m_shader->set_input_assembly_settings({ .m_primitive_topology = ESP_PRIMITIVE_TOPOLOGY_POINT_LIST });
@@ -30,8 +41,11 @@ namespace mg1
         m_shader->set_rasterizer_settings({ .m_line_width = 3.f });
         m_shader->build_worker();
 
-        m_uniform_manager = m_shader->create_uniform_manager(0, 0);
+        m_uniform_manager = m_shader->create_uniform_manager(0, 1);
         m_uniform_manager->build();
+
+        //        CursorParams ubo{ CURSOR_SIZE, CURSOR_COLOR, EspWorkOrchestrator::get_swap_chain_extent_aspect_ratio()
+        //        }; m_uniform_manager->update_buffer_uniform(0, 0, 0, sizeof(CursorParams), &ubo);
       }
     }
 
@@ -39,12 +53,14 @@ namespace mg1
     {
       m_shader->attach();
 
+      // TODO: this shouldn't have to be updated
+      CursorParams ubo{ CURSOR_SIZE, CURSOR_COLOR, EspWorkOrchestrator::get_swap_chain_extent_aspect_ratio() };
+      m_uniform_manager->update_buffer_uniform(0, 0, 0, sizeof(CursorParams), &ubo);
+      //
+
       glm::vec2 mouse_pos{ EspInput::get_mouse_x() / EspWindow::get_width() * 2.f - 1.f,
                            1.f - EspInput::get_mouse_y() / EspWindow::get_height() * 2.f };
-
-      m_uniform_manager->update_buffer_uniform(0, 0, 0, sizeof(glm::vec2), &mouse_pos);
-      glm::mat4 p = glm::mat4(1.f); // Scene::get_current_camera()->get_projection();
-      m_uniform_manager->update_push_uniform(0, &p);
+      m_uniform_manager->update_buffer_uniform(1, 0, 0, sizeof(glm::vec2), &mouse_pos);
 
       m_uniform_manager->attach();
 
